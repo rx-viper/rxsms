4d4620b42f2f8130107a3066698b3bafb3a84275
Thomas <thomas@gruebler.at>
2014-04-26 10:40:02 +0200

    Initial commit

---
+++ src/usart_communication/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc_driver.c
+
+timer_driver.c
+
+main.c
+
+usart_driver.c
+
---
+++ src/usart_communication/usart_communication.c
@@ -0,0 +1,17 @@
+/*
+ * usart_communication.c
+ *
+ * Created: 24.04.2013 10:17:01
+ *  Author: kholodkov
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
