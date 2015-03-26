114ce796f9bdb48648b5febdc64881748ecdb979
beni <beni@beni>
2015-03-26 12:08:07 +0100

    add simple task_debug implementation to dump raw ADC values

--- code/task_debug.c
+++ code/task_debug.c
@@ -18,17 +18,61 @@
 
 #include <avr/io.h>
 #include "task_debug.h"
+#include "task_adc.h"
+
+#define UART_DEBUG_PORT     PORTD
+#define UART_DEBUG_RX_bm    PIN6_bm
+#define UART_DEBUG_TX_bm    PIN7_bm
+#define UART_DEBUG          USARTD1
+#define UART_DEBUG_PWRUP    PR.PRPD &= ~PR_USART1_bm
+
+#define UART_BSEL       (131)
+#define UART_BSCALE     (-3)
 
 static void init(void);
 static void run(void);
 const struct task task_debug = { .init = &init, .run = &run };
 
 static void
+init_uart(PORT_t * port, uint8_t rx_pin, uint8_t tx_pin, USART_t * uart)
+{
+    port->OUTSET = tx_pin;
+    port->DIRSET = tx_pin;
+    port->DIRCLR = rx_pin;
+    uart->CTRLA = 0;
+    uart->CTRLB = USART_RXEN_bm | USART_TXEN_bm;
+    uart->CTRLC = USART_CMODE_ASYNCHRONOUS_gc
+        | USART_PMODE_DISABLED_gc | USART_CHSIZE_8BIT_gc;
+    uart->BAUDCTRLA = (uint8_t) UART_BSEL;
+    uart->BAUDCTRLB = (UART_BSCALE << 4) | (UART_BSEL >> 8);
+}
+
+static void
 init(void)
 {
+    UART_DEBUG_PWRUP;
+    init_uart(&UART_DEBUG_PORT, UART_DEBUG_RX_bm, UART_DEBUG_TX_bm,
+              &UART_DEBUG);
+
 }
 
 static void
 run(void)
 {
+    static uint8_t current_byte = 0;
+    static uint8_t buffer[16];
+
+    if (!(UART_DEBUG.STATUS & USART_DREIF_bm))
+        return;
+
+    UART_DEBUG.DATA = buffer[current_byte];
+
+    ++current_byte;
+    if (current_byte >= sizeof(buffer)) {
+        current_byte = 0;
+        for (uint8_t i = 0; i < sizeof(task_adc_raw) / 2; ++i) {
+            buffer[2 * i + 1] = ((uint8_t*) &task_adc_raw)[2 * i];
+            buffer[2 * i] = ((uint8_t*) &task_adc_raw)[2 * i + 1];
+        }
+    }
 }
